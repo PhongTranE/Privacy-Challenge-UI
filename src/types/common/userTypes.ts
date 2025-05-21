8a6fc8043483e27e05c48e5e86ae8e936932ec73
Phong Trần <phongtranhk20@gmail.com>
2025-05-21 15:57:13 +0700

    Refactor(types): Update UserInterface to use Group interface

--- src/types/common/userTypes.ts
+++ src/types/common/userTypes.ts
@@ -2,12 +2,17 @@ export interface Role {
   name: string;
 }
 
+export interface Group {
+  id: number;
+  name: string;
+}
+
 export interface UserInterface {
   id: number;
   email: string;
   username: string;
   isActive: boolean;
   roles: Role[];
-  group_id: number;
+  group: Group;
   invite_key: string;
 }
